f4dab6e692e312f6e45fe4c61ad16c2417339822
Blagovest Mitsev <blagovest_micev@abv.bg>
2023-01-22 10:56:27 +0200

    Add files via upload

---
+++ StackAndQueuesExercise/BalancedParentheses.java
@@ -0,0 +1,34 @@
+package StackAndQueuesExercise;
+
+import java.util.*;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        String[] input = scanner.nextLine().split("");
+        boolean isBalanced = false;
+
+        for (int i = 0; i < input.length; i++) {
+            String currentBracket = input[i];
+
+            if (currentBracket.equals("[") || currentBracket.equals("{") || currentBracket.equals("(")) {
+                stack.push(currentBracket);
+            } else if (stack.size() > 0 && ((currentBracket.equals("]") && stack.pop().equals("[")) ||
+                    (currentBracket.equals(")") && stack.pop().equals("(")) ||
+                    (currentBracket.equals("}") && stack.pop().equals("{")))) {
+                isBalanced = true;
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        if (isBalanced) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
